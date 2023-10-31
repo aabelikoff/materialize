e7f87ed20a3bba4b3f0fcbf2826ddde4340cf460
Aleksey Belikov <aabelikoff@gmail.com>
2023-10-31 10:44:29 +0300

    Final commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// menu
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+// $('.carousel.carousel-slider').carousel({
+//   fullWidth: true,
+//   indicators: true,
+// });
+
+// $('.carousel.carousel-slider').css('height', '50px');
+// // $('.carousel').css('height', '50px');
+
+// $(document).ready(function () {
+//   $('.materialboxed').materialbox();
+// });
